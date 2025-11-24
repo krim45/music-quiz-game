d15a14efb37ee25be736ea467833dc0c9b3e9a33
kyungbong.kim <kyungbong.kim@aoroa.ai>
2025-11-24 22:05:34 +0900

    fix: Label 컴포넌트 ellipsis 설정

--- frontend/src/components/forms/element/Label.tsx
+++ frontend/src/components/forms/element/Label.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+
 export interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
   label?: string;
   required?: boolean;
@@ -8,10 +10,10 @@ const Label = ({ className = '', id, label = '', required, disabled, ...rest }: 
   if (label.length === 0) return null;
 
   return (
-    <label htmlFor={id} className={`inline-flex h-4 items-center gap-0.5 ${className}`} {...rest}>
+    <label htmlFor={id} className={clsx('flex h-4 min-w-0 flex-1 items-center gap-0.5', className)} {...rest}>
       {required && <span className={`${disabled ? 'text-gray-500' : 'text-red'}`}>*</span>}
 
-      <span className={`${disabled ? 'text-gray-500' : null}`}>{label}</span>
+      <span className={clsx('truncate', { 'text-gray-500': disabled })}>{label}</span>
     </label>
   );
 };

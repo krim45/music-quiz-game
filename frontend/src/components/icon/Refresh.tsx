90ec79efd425107971089abce06de0e550e66d24
kyungbong.kim <kyungbong.kim@aoroa.ai>
2025-12-06 02:42:24 +0900

    feat: Refresh Icon

---
+++ frontend/src/components/icon/Refresh.tsx
@@ -0,0 +1,17 @@
+import { IconProps } from '@/types/icon';
+
+const Refresh = ({ className = '', size = 20, fill = 'currentColor', ...rest }: IconProps) => {
+  return (
+    <span className={className} {...rest}>
+      <svg xmlns='http://www.w3.org/2000/svg' width={size} height={size} viewBox='0 0 24 24' fill='none'>
+        <title>refresh</title>
+        <path
+          d='M17.65,6.35C16.2,4.9 14.21,4 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20C15.73,20 18.84,17.45 19.73,14H17.65C16.83,16.33 14.61,18 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6C13.66,6 15.14,6.69 16.22,7.78L13,11H20V4L17.65,6.35Z'
+          fill={fill}
+        />
+      </svg>
+    </span>
+  );
+};
+
+export default Refresh;

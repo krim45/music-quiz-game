145e9e0604882c7b524e65c9468a36d2fd414672
kyungbong.kim <kyungbong.kim@aoroa.ai>
2026-01-26 16:38:06 +0900

    feat: enter input focus & scrollToEnd

--- frontend/src/app/room/[roomId]/_components/ChatRoom.tsx
+++ frontend/src/app/room/[roomId]/_components/ChatRoom.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
-import { ChatMessage } from '@/types/game';
+import { isTypingElement } from '@/utils/reactUtils';
 
 import MessageItem from '@/app/room/[roomId]/_components/MessageItem';
 import BaseInput from '@/components/form/input/BaseInput';
 import Button from '@/components/button/Button';
+import Send from '@/components/icon/Send';
+import ChevronDown from '@/components/icon/ChevronDown';
 
-// TODO: enter input 포커스
-// ? 스크롤 이벤트 스로틀 디바운스
+import { ChatMessage } from '@/types/game';
+
+// TODO
+// 채팅창 가상 스크롤
+// 스크롤 이벤트 스로틀 디바운스
 
 interface Props {
   messages: ChatMessage[];
@@ -16,10 +21,11 @@ interface Props {
 export default function ChatRoom({ messages, onSendMessage }: Props) {
   const [input, setInput] = useState('');
   const [isUserScrolledUp, setIsUserScrolledUp] = useState(false);
+
   const containerRef = useRef<HTMLDivElement | null>(null);
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
-  // 유저 스크롤 감지
   useEffect(() => {
     const container = containerRef.current;
     if (!container) return;
@@ -31,36 +37,54 @@ export default function ChatRoom({ messages, onSendMessage }: Props) {
       setIsUserScrolledUp(!isBottom);
     };
 
+    const onEnterFocus = (e: KeyboardEvent) => {
+      if (e.key !== 'Enter') return;
+
+      if (e.isComposing) return;
+
+      console.log(e.target);
+      if (isTypingElement(document.activeElement)) return;
+
+      e.preventDefault();
+      e.stopPropagation();
+      inputRef.current?.focus();
+    };
+
     container.addEventListener('scroll', handleScroll);
-    return () => container.removeEventListener('scroll', handleScroll);
+    window.addEventListener('keydown', onEnterFocus);
+    return () => {
+      container.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('keydown', onEnterFocus);
+    };
   }, []);
 
-  // 메시지 추가될 때 자동 스크롤
   useEffect(() => {
     if (isUserScrolledUp) return;
 
-    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    scrollToMessageEnd();
   }, [messages, isUserScrolledUp]);
 
-  // 메시지 전송
   const sendMessage = () => {
     const message = input.trim();
+    if (!message) return;
+
     onSendMessage?.(message);
     setInput('');
   };
 
+  const scrollToMessageEnd = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
-    <section className='flex min-h-0 flex-1 flex-col overflow-hidden rounded border border-green-900'>
-      {/* TODO: 가상 스크롤 적용 */}
-      {/* isUserScrolledUp이 true면 채팅창 가장 아래로 보내는 아이콘 적용 */}
-      <div className='scrollbar-custom min-h-0 flex-1 overflow-y-auto px-2' ref={containerRef}>
+    <section className='relative flex min-h-0 flex-1 flex-col overflow-hidden rounded border border-green-900'>
+      <div ref={containerRef} className='scrollbar-custom min-h-0 flex-1 overflow-y-scroll pr-1 pl-2'>
         {messages.map((message, idx) => (
           <MessageItem key={idx} message={message} />
         ))}
         <div ref={messagesEndRef} />
       </div>
 
-      {/* enter로 input 포커스 => 이거를 위해서 좀 복잡해짐, 케이스 잘 생각해서 고려 */}
       <form
         className='flex flex-none items-center gap-2 border-t border-green-900 p-2'
         onSubmit={(e) => {
@@ -69,17 +93,25 @@ export default function ChatRoom({ messages, onSendMessage }: Props) {
         }}
       >
         <BaseInput
+          ref={inputRef}
           className='flex-1 rounded border border-green-900 p-1 text-sm focus:border-green-600'
           value={input}
           onChange={(v) => setInput(v)}
           placeholder='정답을 입력하세요'
         />
 
-        {/* TODO: 아이콘으로 대체 */}
         <Button type='submit' size='sm' color='green'>
-          보내기
+          <Send />
         </Button>
       </form>
+
+      {isUserScrolledUp && (
+        <ChevronDown
+          className='absolute right-4 bottom-14 cursor-pointer rounded-full bg-gray-300 text-gray-700 hover:bg-white'
+          size={28}
+          onClick={scrollToMessageEnd}
+        />
+      )}
     </section>
   );
 }
--- frontend/src/utils/reactUtils.tsx
+++ frontend/src/utils/reactUtils.tsx
@@ -1,3 +1,17 @@
 export function isUpdater<T>(action: React.SetStateAction<T>): action is (prev: T) => T {
   return typeof action === 'function';
 }
+
+export function isTypingElement(el: Element | null) {
+  if (!el) return false;
+
+  const tag = el.tagName.toLowerCase();
+
+  return (
+    tag === 'input' ||
+    tag === 'textarea' ||
+    tag === 'select' ||
+    tag === 'button' ||
+    (el as HTMLElement).isContentEditable
+  );
+}

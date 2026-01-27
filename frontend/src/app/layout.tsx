13ec96773af8173f57271644fbbb27a92d17712d
kyungbong.kim <kyungbong.kim@aoroa.ai>
2026-01-27 18:51:02 +0900

    feat: G-Tag

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,12 @@
+import type { Metadata } from 'next';
+import Script from 'next/script';
+
 import ToastProvider from '@/components/feedback/ToastProvider';
 import Provider from '@/lib/TanstackQueryProvider';
 import '@/styles/global.css';
-import type { Metadata } from 'next';
 
 const siteUrl = process.env.NEXT_PUBLIC_SITE_URL!;
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID!;
 const title = '노래 맞추기 게임';
 
 export const metadata: Metadata = {
@@ -62,7 +65,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        {/* GTM - head */}
+        <Script id='gtm' strategy='afterInteractive'>
+          {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `}
+        </Script>
+      </head>
       <body className='antialiased'>
+        {/* GTM - noscript body 바로 아래 권장 */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height='0'
+            width='0'
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Provider>
           <main className='h-full w-full'>{children}</main>
         </Provider>

96edc740e6520f7c694eee4661d0b61810d3eb10
kyungbong.kim <kkbong365@gmail.com>
2026-02-01 18:36:58 +0900

    Revert "test: LCP 측정"
    This reverts commit dbd6d95a4ae85d7e26bda4ab8cb702fd80628e5f.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  allowedDevOrigins: ['192.168.219.152', 'localhost:3000'],
 };
 
 export default nextConfig;
